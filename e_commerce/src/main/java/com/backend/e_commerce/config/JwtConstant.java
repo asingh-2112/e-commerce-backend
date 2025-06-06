f6d7efab6e359c602909ddd3bff8ba2af1333f52
asingh-2112 <aksinghrbj2112@gmail.com>
2025-06-06 20:47:04 +0530

    Added Security and Jwt

---
+++ e_commerce/src/main/java/com/backend/e_commerce/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.backend.e_commerce.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "secret";
+    public static final String JWT_HEADER = "Authorization";
+}
