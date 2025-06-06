9abf62b851889ec2f58ffa5ce2b15c83245ad432
asingh-2112 <aksinghrbj2112@gmail.com>
2025-06-06 21:48:24 +0530

    Updated secret key

--- e_commerce/src/main/java/com/backend/e_commerce/config/JwtConstant.java
+++ e_commerce/src/main/java/com/backend/e_commerce/config/JwtConstant.java
@@ -1,6 +1,6 @@
 package com.backend.e_commerce.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY = "secret";
+    public static final String SECRET_KEY = "e2c7b9af9f38462abe23f7d0754fa1c8";
     public static final String JWT_HEADER = "Authorization";
 }
